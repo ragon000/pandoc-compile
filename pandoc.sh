95ab4b7dbd4edb9e37fbfaa1c56108b34543ebc6
Philipp Hochkamp <philipp.hochkamp@tu-dortmund.de>
2019-01-14 15:25:55 +0100

    paralell and path stuff stuff

--- pandoc.sh
+++ pandoc.sh
@@ -5,7 +5,7 @@ echo "i am $(whoami) with id $(id -g)"
 if [ ! $PANDOCCMD ]
 then
   echo "pandoc.sh no PANDOCCMD found, setting default one"
-  PANDOCCMD="pandoc --filter pandoc-include-code --pdf-engine=pdflatex"
+  export PANDOCCMD="pandoc --filter pandoc-include-code --pdf-engine=pdflatex"
 fi
 
 if [ $TEMPLATELINK ]
@@ -17,17 +17,20 @@ then
 fi
 
 compile () {
-      echo $PANDOCCMD $1 -o build/${f%.*}.pdf
-      mkdir -p ./build/${1%/*}
-      $PANDOCCMD $1 -o ./build/${1%.*}.pdf
+      mkdir -p build/${1%/*}
+      OLDPWD=$PWD
+      pushd ${1%/*}
+      $PANDOCCMD $(basename $1) -o $OLDPWD/build/${1%.*}.pdf
+      echo $PANDOCCMD $(basename $1) -o $OLDPWD/build/${1%.*}.pdf
+      popd
 }
 
 
 
-if [ ! -d "/build" ]; then
+if [ ! -d "build" ]; then
   echo "pandoc.sh /build not found, generating"
-  mkdir /build
+  mkdir build
 fi
 
 export -f compile
-paralell compile ::: $(find . -name '*.md*')
+parallel compile ::: $(find . -name '*.md*')
